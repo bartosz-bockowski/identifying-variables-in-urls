3917dc63393b23e6ad90fe96f4a6f070e05374b9
Bartek Boćkowski <bboc12357@gmail.com>
2023-12-29 18:01:24 +0100

    optimization

--- src/main/java/pl/bartek/Main.java
+++ src/main/java/pl/bartek/Main.java
@@ -1,6 +1,7 @@
 package pl.bartek;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,19 +13,19 @@ public class Main {
         ));
 
         List<List<String>> parts = urls.stream()
-                        .map(url -> List.of(url.split("/")).stream()
-                                .filter(url1 -> !url1.isBlank())
+                        .map(url -> Stream.of(url.split("/"))
+                                .filter(url_ -> !url_.isBlank())
                                 .toList())
                 .toList();
 
-        List<String> url1Parts = parts.get(0);
-        int size = url1Parts.size();
+        List<String> firstUrlParts = parts.get(0);
+        int size = firstUrlParts.size();
         List<Boolean> isVariableList = new ArrayList<>();
 
         for(int i = 0; i < size; i++){
             boolean isVariable = false;
             for(int j = 1; j < parts.size(); j++){
-                if(!Objects.equals(parts.get(j).get(i), url1Parts.get(i))){
+                if(!Objects.equals(parts.get(j).get(i), firstUrlParts.get(i))){
                     isVariable = true;
                     break;
                 }
@@ -34,7 +35,7 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < size; i++){
-            sb.append("/").append(isVariableList.get(i) ? "**variable**" : url1Parts.get(i));
+            sb.append("/").append(isVariableList.get(i) ? "**variable**" : firstUrlParts.get(i));
         }
 
         System.out.println(sb);
